90afab631b44bcc216ddd071161a3a45c5679837
David FletcherE <dtsfletcher@gmail.com>
2017-08-16 17:15:27 -0400

    done

--- products.rb
+++ products.rb
@@ -1,18 +1,16 @@
-class Products
+class Product
 
-  def initialize(name, base_price, tax_rate)
-    @name = name
-    @base_price = base_price
-    @tax_rate = tax_rate
+ def initialize(name,base_price,tax_rate)
+   @name = name
+   @base_price = base_price
+   @tax_rate = tax_rate
   end
 
-  def total_price
-    @total_price = @base_price * (1 + @tax_rate)
+ def base_price
+    @base_price
+  end
+
+ def total_price
+    @total_price = @base_price * ( 1 + @tax_rate )
   end
 end
-
-
-product1 = Products.new("phone", 100, 0.50)
-
-puts product1.inspect
-p product1.total_price
